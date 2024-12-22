b9280b5d5fe74db3540a4584b90c9720596056ac
Facalder <faainamacaldera16@gmail.com>
2024-12-22 09:39:13 +0700

    feat(Init): Initialize Project for AKA Final Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button, DarkThemeToggle, Flowbite } from "flowbite-react";
+
+export default function App() { 
+  return ( 
+    <>
+        <Flowbite>
+          <DarkThemeToggle/>
+        </Flowbite>
+    </>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const flowbite = require("flowbite-react/tailwind")
+
+module.exports = {
+  content: [
+    'index.html',
+    './src/**/*.{js,jsx,ts,tsx,vue,html}',
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    flowbite.plugin()
+  ],
+}
