a4dbf93e5b0d057e577dd4ea3e5bf51d34a37de0
Tdarrelpa <darrel2208@gmail.com>
2024-12-24 16:46:00 +0700

    feat(LogicChart.jsx): Agak takut commit yang ini, cobain dulu

--- src/components/LogicChart.jsx
+++ src/components/LogicChart.jsx
@@ -1,123 +1,110 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import ReactApexChart from "react-apexcharts";
 
 export default function LogicChart() {
     const inputSize = [20, 7500, 1000, 10000, 100000];
-    const executionTimesIterative = [];
-    
-    const executionTimesRecursive = [];
+    const [executionTimesIterative, setExecutionTimesIterative] = useState([]);
+    const [executionTimesRecursive, setExecutionTimesRecursive] = useState([]);
 
-    const testPerformance = () => { 
-      inputSize.forEach(size => { 
-        const startTime = performance.now()
-        
-        const players = randomizePlayer(size)
-        createBalancedTeamIterative(players)
+    useEffect(() => {
+        testPerformance();
+        testPerformanceRecursive();
+    }, []);
 
-        const endTime = performance.now()
-        const timeElapsed = endTime - startTime
+    const testPerformance = () => {
+        const times = inputSize.map(size => {
+            const startTime = performance.now();
+            const players = randomizePlayer(size);
+            createBalancedTeamIterative(players);
+            const endTime = performance.now();
+            return endTime - startTime;
+        });
+        setExecutionTimesIterative(times);
+    };
 
-        executionTimesIterative.push(timeElapsed);
-      })
-    }
+    const testPerformanceRecursive = () => {
+        const times = inputSizes.map(size => {
+            const startTime = performance.now();
+            const players = randomizePlayer(size);
+            createBalancedTeamRecursive(players);
+            const endTime = performance.now();
+            return endTime - startTime;
+        });
+        setExecutionTimesRecursive(times);
+    };
 
-    const testPerformanceRecursive = () => { 
-        const startTime = performance.now();
-
-        const players = randomizePlayer(size);
-        createBalancedTeamRecursive(players);
-
-        const endTime = performance.now();
-        const timeElapsed = endTime - startTime;
-
-        executionTimesRecursive.push(timeElapsed);
-    }
-    
-    const [chartState, setChartState] = useState({
-      series: [
-        {
-          name: "Iterative",
-          data: inputSize,
+    const chartState = {
+        series: [
+            {
+                name: "Iterative",
+                data: executionTimesIterative,
+            },
+            {
+                name: "Recursive",
+                data: executionTimesRecursive,
+            },
+        ],
+        options: {
+            chart: {
+                height: 350,
+                type: "line",
+                stacked: false,
+                zoom: {
+                    enabled: true,
+                    type: "x",
+                    autoScaleYaxis: true,
+                },
+            },
+            toolbar: {
+                show: false,
+            },
+            colors: ["#77B6EA", "#545454"],
+            dataLabels: {
+                enabled: true,
+            },
+            stroke: {
+                curve: "smooth",
+            },
+            title: {
+                text: "Running Time for Each Algorithm",
+                align: "left",
+            },
+            grid: {
+                borderColor: "#e7e7e7",
+                row: {
+                    colors: ["#f3f3f3", "transparent"],
+                    opacity: 0.5,
+                },
+            },
+            markers: {
+                size: 1,
+            },
+            xaxis: {
+                categories: inputSizes,
+                title: {
+                    text: "Input Size",
+                },
+            },
+            yaxis: {
+                title: {
+                    text: "Running Time (ms)",
+                },
+            },
+            legend: {
+                position: "top",
+                horizontalAlign: "right",
+                floating: true,
+                offsetY: -25,
+                offsetX: -5,
+            },
         },
-        {
-          name: "Recursive",
-          data: inputSize,
-        },
-      ],
-
-      options: {
-        chart: {
-          height: 350,
-          type: "line",
-          stacked: false,
-
-          zoom: {
-            enabled: true,
-            type: "x",
-            autoScaleYaxis: true,
-          },
-        },
-
-        toolbar: {
-          show: false,
-        },
-
-        colors: ["#77B6EA", "#545454"],
-
-        dataLabels: {
-          enabled: true,
-        },
-
-        stroke: {
-          curve: "smooth",
-        },
-
-        title: {
-          text: "Running Time for Each Algorithms",
-          align: "left",
-        },
-
-        grid: {
-          borderColor: "#e7e7e7",
-          row: {
-            colors: ["#f3f3f3", "transparent"],
-            opacity: 0.5,
-          },
-        },
-
-        markers: {
-          size: 1,
-        },
-
-        xaxis: {
-          categories: inputSize,
-          title: {
-            text: "Input Size",
-          },
-        },
-
-        yaxis: {
-          title: {
-            text: "Running Time",
-          },
-          categories: [executionTimesIterative, executionTimesRecursive],
-        },
-
-        legend: {
-          position: "top",
-          horizontalAlign: "right",
-          floating: true,
-          offsetY: -25,
-          offsetX: -5,
-        },
-      },
-    });
+    };
 
     return (
         <>
             <ReactApexChart options={chartState.options} type="line" series={chartState.series} height={320} />
         </>
-    )
+    );
 }
